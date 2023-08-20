e89935c976d5ad9af1aa6c548149dff61a903453
Takos4Lunch <takeshie01@gmail.com>
2023-08-20 11:39:25 -0400

    notes route

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 const authRouter = require('./auth.route')
 const userRouter = require('./user.route');
+const notesRouter = require('./notes.route');
 
 function routerApi(app){
     app.use('/auth', authRouter);
     app.use('/user', userRouter);
+    app.use('/notes', notesRouter);
 }
 
 module.exports = routerApi;
---
+++ routes/notes.route.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const passport = require('passport');
+const NotesController = require('../controllers/note.controller');
+const { checkRoles } = require('../middlewares/auth.handler');
+
+const router = express.Router();
+//controller object
+const controller = new NotesController();
+
+router.get('/', passport.authenticate('jwt', {session: false}), checkRoles('admin'), 
+async(req, res, next) => {
+    try{
+        const user = await controller.findAll();
+        res.json(user);
+    } catch(err){
+        next(err);
+    }
+})
+
+router.get('/:id', passport.authenticate('jwt', {session: false}), checkRoles('admin', 'user'), async (req, res, next) => {
+    try{
+        const user = await controller.findById(req.params.id);
+        res.json(user);
+    } catch(err){
+        next(err);
+    }
+})
+
+router.post('/', /*passport.authenticate('jwt', {session: false}), checkRoles('admin') ,*/
+async(req, res, next) => {
+    try{
+        const body = req.body;
+        const user = await controller.create(body);
+        return res.json(user);
+    } catch(err){
+        next(err)
+    }
+})
+
+router.patch('/:id', passport.authenticate('jwt', {session:false}), checkRoles('admin', 'user'), 
+async(req, res) => {
+    try{
+        const {id} = req.params;
+        const body = req.body;
+        const changes = await controller.update(id, body);
+        res.json(changes)
+    }catch(err){
+        next(err)
+    }
+})
+
+router.delete('/:id', passport.authenticate('jwt', {session:false}), checkRoles('admin'), async(req, res) => {
+    try{
+        const {id} = req.params;
+        const result = await controller.delete(id)
+        res.json(result)
+    }catch(err){
+        next(err)
+    }
+})
+
+module.exports = router;
