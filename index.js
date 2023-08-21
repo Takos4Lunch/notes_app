a56c1aebddf9dc1377960a4dc4a10c5d93327860
Takos4Lunch <takeshie01@gmail.com>
2023-08-21 00:41:14 -0400

    initialized documentation

--- index.js
+++ index.js
@@ -4,10 +4,36 @@ const sequelize = require('./libs/sequelize')
 const routerApi = require('./routes');
 const { logErrors, errorHandler, boomErrorHandler, ormErrorHandler } = require('./middlewares/error.handler');
 
+//Documentation
+const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+
+const swaggerDefinition = {
+  openapi: '3.0.0',
+  info: {
+    title: 'Notes API',
+    version: '1.0.0',
+    description: 'This is a REST API that allows user to register and manage personal notes',
+    license: {
+        name: 'Licensed Under MIT',
+        url: 'https://spdx.org/licenses/MIT.html',
+    },
+  },
+};
+
+const options = {
+  swaggerDefinition,
+  // Paths to files containing OpenAPI definitions
+  apis: ['./routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+//Documentation
+
 const port = 3000;
 
 app.use(express.json());
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 require('./utils/auth');
 
 routerApi(app);
--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,11 +5,58 @@ const jwt = require('jsonwebtoken');
 const config = require('../config/config');
 
 /**
- * @summary Endpoint used for authentication
- * The request body must consist of the following parameters:
- * @param { string } email : user's email address
- * @param { string } password : user's password
- * @yields { jsonwebtoken } 
+ * @swagger
+ * /auth/login:
+ *   post:
+ *     summary: Endpoint for authentication, the user must provide their email and password
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               email:
+ *                 type: string
+ *                 description: user's email
+ *                 example: jhonexample@email.com
+ *               password:
+ *                 type: string
+ *                 description: user's password
+ *                 example: bigpassword
+ *     responses:
+ *       200:
+ *         description: A Json object containing the data of the user, and their generated JSON web token.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 user:
+ *                   type: array
+ *                   items:
+ *                     type: object
+ *                     properties:
+ *                       id:
+ *                         type: integer
+ *                         description: The user ID.
+ *                         example: 0
+ *                       role:
+ *                         type: string
+ *                         description: User's role
+ *                         example: user
+ *                       username:
+ *                         type: string
+ *                         description: User's nickname.
+ *                         example: johnexample
+ *                       email:
+ *                         type: string
+ *                         description: User's email address.
+ *                         example: johnexample@email.com
+ *                 token:
+ *                   type: string
+ *                   description: Generated token for user authentication
+ *                   example: eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInJvbGUiOiJ1c2VyIiwiaWF0IjoxNjkyNTg5OTE2fQ.dxc0bTzxskOAHkM9_Q2mfmRybtOs9b6q1rUzINgS03A
  */
 router.post('/login', passport.authenticate('local', {session: false}) , async (req, res, next) => {
     try {
